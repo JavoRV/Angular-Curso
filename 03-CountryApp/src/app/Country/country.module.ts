2da28f6d37accd59e4d7dc3e6d57ef618d7326ab
JavoRVA <javierazaid1mail.com>
2023-01-10 11:09:26 -0600

    Countries

---
+++ 03-CountryApp/src/app/Country/country.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
+
+
+
+@NgModule({
+  declarations: [
+    ByCapitalComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountryModule { }
